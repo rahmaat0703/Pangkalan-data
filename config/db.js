e2a68dd5afb8fb142bad9d3939855b821265eb5a
Rahmat <rahmatktb11@gmail.com>
2026-08-14 12:10:09 +0700

    Fix Railway MySQL connection

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 const mysql = require('mysql2');
 
 const pool = mysql.createPool(
-    'mysql://root:bmhMsLqeZifHnCKAcZkfolEXvNCQopJY@hayabusa.proxy.rlwy.net:38955/railway'
+    process.env.MYSQL_URL
 );
 
 const promisePool = pool.promise();
