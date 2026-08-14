68afdd8518d196f831c0f8023d29da42d3fc906f
Rahmat <rahmatktb11@gmail.com>
2026-08-14 12:00:19 +0700

    Connect database to Railway

--- config/db.js
+++ config/db.js
@@ -1,10 +1,11 @@
 const mysql = require('mysql2');
 
 const pool = mysql.createPool({
-    host: 'localhost',
+    host: 'mysql.railway.internal',
+    port: 3306,
     user: 'root',
-    password: '',
-    database: 'pelayanan_data',
+    password: 'bmhMsLqeZifHnCKAcZkfolEXvNCQopJY',
+    database: 'railway',
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
