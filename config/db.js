0e04911ea2d1132ac784c920cae22f5080be820f
rahmaat0703 <rahmatktb11@gmail.com>
2026-08-14 13:05:59 +0700

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,17 +1,7 @@
 const mysql = require('mysql2');
 
-console.log('================================');
-console.log('🔌 DATABASE CONNECTION');
-console.log('================================');
-console.log('Host     : altaria.proxy.rlwy.net');
-console.log('Port     : 46732');
-console.log('Database : railway');
-console.log('User     : root');
-console.log('Connecting...');
-
 const pool = mysql.createPool({
-    host: 'altaria.proxy.rlwy.net',
-    port: 46732,
+    host: 'mysql.railway.internal',
     user: 'root',
     password: 'uiChLwFleUFCOcKKRtSVuqDYRdxwkbxz',
     database: 'railway',
@@ -21,21 +11,5 @@ const pool = mysql.createPool({
 });
 
 const promisePool = pool.promise();
-
-promisePool.getConnection()
-    .then(connection => {
-        console.log('================================');
-        console.log('✅ DATABASE CONNECTED');
-        console.log('================================');
-
-        connection.release();
-    })
-    .catch(error => {
-        console.log('================================');
-        console.log('❌ DATABASE CONNECTION FAILED');
-        console.log('Code    :', error.code);
-        console.log('Message :', error.message);
-        console.log('================================');
-    });
 
 module.exports = promisePool;
